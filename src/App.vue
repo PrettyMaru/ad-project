9050e05b5a54d5e11f9204a2f8bbacce77188486
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2021-10-29 02:04:14 +0300

    2.5 Dynamic menu

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,17 @@
                 </v-list-item-content>
             </v-list-item>
         </v-list-item-group>
+        <v-list-item
+            v-for="link in links"
+            :key="link.title"
+            >
+                <v-list-item-icon>
+                    <v-icon>{{ link.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>{{ link.title }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
     </v-list>
     </v-navigation-drawer>
     
@@ -32,6 +43,10 @@
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn
+            v-for="link in links"
+            :key="link.title"
+            text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
             <v-btn text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn>
             <v-btn text>Link Two</v-btn>
             <v-btn text>Link Three</v-btn>
@@ -47,7 +62,15 @@
 export default {
  data() {
      return {
-         drawer: false
+         drawer: false,
+         links: [
+             {title:"Login", icon:"mdi-lock", url:"/Login"},
+             {title:"Registration", icon:"mdi-face", url:"/Registration"},
+                {title:"Orders", icon:"mdi-bookmark-multiple-outline",
+                url:"/Orders"},
+            {title:"New ad", icon:"mdi-note-plus-outline", url:"/New"},
+            {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+         ]
      }
  }
 }
