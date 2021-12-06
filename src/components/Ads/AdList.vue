20c540f25a35bdaa14b276b92521be232813a61c
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2021-12-06 19:35:56 +0300

    8.2 Подключение данных к списку

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -3,22 +3,23 @@
         <v-layout row>
             <v-flex xs12 sm6 offset-sm3> 
                 <h1 class="text--secondary mb-3 mt-3">My ads</h1>
-            <v-card class="elevation-10 mb-5">
+            <v-card class="elevation-10 mb-5" v-for="ad in ads" :key="ad.id">            
                 <v-layout row>
                     <v-flex xs4 class="pl-3">
-                        <v-img
+                        <v-img :src="ad.src" 
                         height="175px">
                         </v-img>
                     </v-flex>
                     <v-flex xs8 class="pr-3">
                         <v-card-text>
-                            <h2 class="text--primary">{{}}</h2>
-                            <p>{{ }}</p>
+                            <h2 class="text--primary">{{ad.title}}</h2>
+                            <p>{{ad.desc}}</p>
                           </v-card-text>
                           <v-card-actions>
                           <v-spacer></v-spacer>
                           <v-btn 
-                          color="primary">
+                          color="primary" 
+                          :to="'/ad/' + ad.id">
                               Open
                           </v-btn>
                           </v-card-actions>                  
@@ -34,6 +35,36 @@
 export default {
     data() {
         return {
+            ads:[
+	{
+	title:"First",
+	desc:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+	promo: true,
+	src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+	id:"1"
+	},
+	{
+	title:"Second",
+	desc:"Second Desc",
+	promo: true,
+	src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+	id:"2"
+	},
+	{
+	title:"Third",
+	desc:"Thitd Desc",
+	promo: true,
+	src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+	id:"3"
+	},
+	{
+	title:"Fouth",
+	desc:"Fouth Desc",
+	promo: true,
+	src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+	id:"4"
+	}
+	]
         }
     }
 }
