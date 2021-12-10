fa7e94bd31de73abe79c1224e74fc2f292afbeb1
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2021-12-10 12:51:31 +0300

    16.2 Логика для отображения ошибок

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,7 @@
   <v-main>
     <router-view></router-view>
   </v-main>
+  <template v-if="error">
   <v-snackbar
   :timeout="5000"
   :multi-line="true"
@@ -60,8 +61,10 @@
   @input="closeError"
   :value="true"
   >
+    {{ error }}
   <v-btn text dark @click.native="closeError">Close</v-btn>
   </v-snackbar>
+  </template>
   </v-app>
 </template>
 
@@ -83,7 +86,13 @@ export default {
     closeError () {
       this.$store.dispatch('clearError')
     }
- }
+ },
+
+  computed: {
+    error () {
+      return this.$store.getters.error
+   }
+}
 }
 </script>
 <style scoped>
