de01593466948d602e4bf517123aa8a089cb9d4e
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2021-12-11 16:34:37 +0300

    17.2 Управление пунктами меню для залогиненного или незалогиненного пользователя

--- src/App.vue
+++ src/App.vue
@@ -73,27 +73,41 @@ export default {
   data() {
     return {
       drawer: false,
-      links: [
-      {title:"Login", icon:"mdi-lock", url:"/login"},
-      {title:"Registration", icon:"mdi-face", url:"/registration"},
-      {title:"Orders", icon:"mdi-bookmark-multiple-outline", url:"/orders"},
-      {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
-      {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
-      ]
-  }
-},
+      
+    }
+  },
   methods: {
     closeError () {
-      this.$store.dispatch('clearError')
+			this.$store.dispatch('clearError')
+		},
+    onLogout () {
+      this.$store.dispatch('logoutUser')
+  this.$router.push("/")
     }
- },
-
+  },
   computed: {
     error () {
       return this.$store.getters.error
-   }
-}
-}
+    },
+    isUserLoggedIn () {
+      return this.$store.getters.isUserLoggedIn
+    },
+    links () {
+      if (this.isUserLoggedIn) {
+        return [
+        {title:"Orders", icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+        {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
+        {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+        ]
+      } else {
+        return [
+        {title:"Login", icon:"mdi-lock", url:"/login"},
+        {title:"Registration", icon:"mdi-face", url:"/registration"},
+        ]
+      }
+    }
+  }
+};
 </script>
 <style scoped>
   .pointer {
